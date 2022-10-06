528d65d3664aa5422eb5a54a2685e8c4e284a5cd
shorif <shorifpatwary999@gmail.com>
2022-10-06 13:54:11 +0600

    varialbe scss file updated

---
+++ wordpress/wordpress dynamic logo .php
@@ -0,0 +1,21 @@
+		<!-- Your site title as branding in the menu  -->
+		<?php if (! has_custom_logo()) { ?>
+
+		<?php if (is_front_page() && is_home()) : ?>
+
+		<h1 class="navbar-brand mb-0"><a rel="home"
+		        href="<?php echo esc_url(home_url('/')); ?>"
+		        itemprop="url"><?php bloginfo('name'); ?></a></h1>
+
+		<?php else : ?>
+
+		<a class="navbar-brand" rel="home"
+		    href="<?php echo esc_url(home_url('/')); ?>"
+		    itemprop="url"><?php bloginfo('name'); ?></a>
+
+		<?php endif; ?>
+
+		<?php
+		} else {
+		    the_custom_logo();
+		}
